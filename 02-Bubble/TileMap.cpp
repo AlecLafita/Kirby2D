87e1cf2ddcc66cc410b386bc6e4ee87416aeb478
LafitaH <AlecLafitarf@gmail.com>
2016-10-03 19:52:20 +0200

    [WIP] Not getting tiles correctly

--- 02-Bubble/TileMap.cpp
+++ 02-Bubble/TileMap.cpp
@@ -121,8 +121,9 @@ void TileMap::prepareArrays(const glm::vec2 &minCoords, ShaderProgram &program)
 				// Non-empty tile
 				nTiles++;
 				posTile = glm::vec2(minCoords.x + i * tileSize, minCoords.y + j * tileSize);
-				texCoordTile[0] = glm::vec2(float((tile-1)%2) / tilesheetSize.x, float((tile-1)/2) / tilesheetSize.y);
+				texCoordTile[0] = glm::vec2(float((tile - 1) % tilesheetSize.x) / tilesheetSize.x, float((tile - 1) / tilesheetSize.x) / tilesheetSize.y);
 				texCoordTile[1] = texCoordTile[0] + tileTexSize;
+				cout << halfTexel.x << " " << halfTexel.y << endl;
 				//texCoordTile[0] += halfTexel;
 				texCoordTile[1] -= halfTexel;
 				// First triangle
